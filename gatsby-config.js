296bc5f36187901b233ae6cf158ffaeab68d5e57
Roger Coverzola <diolink.4103@gmail.com>
2021-01-19 20:26:55 -0300

    Add dependecies gatsby-plugin-react-svg

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,26 +1,26 @@
 require('dotenv').config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
 const contentfulConfig = {
   spaceId: process.env.CONTENTFUL_SPACE_ID,
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-}
+};
 
 // if you want to use the preview API please define
 // CONTENTFUL_HOST in your environment config
 // the `host` property should map to `preview.contentful.com`
 // https://www.contentful.com/developers/docs/references/content-preview-api/#/reference/spaces/space/get-a-space/console/js
 if (process.env.CONTENTFUL_HOST) {
-  contentfulConfig.host = process.env.CONTENTFUL_HOST
+  contentfulConfig.host = process.env.CONTENTFUL_HOST;
 }
 
-const { spaceId, accessToken } = contentfulConfig
+const { spaceId, accessToken } = contentfulConfig;
 
 if (!spaceId || !accessToken) {
   throw new Error(
-    'Contentful spaceId and the access token need to be provided.'
-  )
+    'Contentful spaceId and the access token need to be provided.',
+  );
 }
 
 module.exports = {
@@ -37,5 +37,13 @@ module.exports = {
       resolve: 'gatsby-source-contentful',
       options: contentfulConfig,
     },
+    {
+      resolve: 'gatsby-plugin-react-svg',
+      options: {
+        rule: {
+          include: /assets/, // See below to configure properly
+        },
+      },
+    },
   ],
-}
+};
