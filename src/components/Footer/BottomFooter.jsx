68cdfa3bf74c64003f66d2d656758cf024e76cd6
Roger BC <diolink.4103@gmail.com>
2021-03-26 13:36:53 -0300

    Youtube icon is added

--- src/components/Footer/BottomFooter.jsx
+++ src/components/Footer/BottomFooter.jsx
@@ -1,65 +1,25 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import { Typography, Grid } from '@material-ui/core';
+import { Typography, Grid, Tooltip } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import FacebookIcon from '@material-ui/icons/Facebook';
 import InstagramIcon from '@material-ui/icons/Instagram';
 import GitHubIcon from '@material-ui/icons/GitHub';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 const useStyles = makeStyles((theme) => ({
-  root: { margin: 0, padding: 0, maxWidth: 'none' },
-  icon: { width: 100, height: 100 },
-  toolbar: {
-    boxShadow: 'inset 0 2px 3px #ccc',
-    padding: theme.spacing(4),
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    color: theme.palette.main,
-    background: theme.palette.primary.contrastText,
-    flexWrap: 'wrap',
-    '@media (max-width: 576px)': { flexDirection: 'column' },
+  bottomBar: {
+    height: 9 * theme.spacing(5),
+    background: theme.palette.primary.main,
+    color: theme.palette.primary.contrastText,
+    padding: theme.spacing(5),
   },
   section: {
-    flex: 1,
-    margin: theme.spacing(4),
     minWidth: 100,
-  },
-  sectionTitle: {
-    color: theme.palette.primary.light,
-    fontSize: '18px',
-    fontWeight: 'bold',
-  },
-  links: {
-    display: 'flex',
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    maxWidth: 300,
-    '@media (max-width: 576px)': { flexDirection: 'column' },
   },
   link: {
     textDecoration: 'none',
     cursor: 'pointer',
-  },
-  linkText: {
-    color: 'gray',
-    paddingTop: 5,
-    fontSize: '14px',
-    fontFamily: theme.typography.fontFamily,
-  },
-  formContainer: {
-    paddingTop: 10,
-    maxWidth: 300,
-  },
-  bottomBar: {
-    background: theme.palette.primary.main,
-    color: theme.palette.primary.contrastText,
-    display: 'flex',
-    alignItems: 'center',
-    padding: theme.spacing(8),
-    flexWrap: 'wrap',
   },
   bottomText: {
     fontSize: '14px',
@@ -67,11 +27,7 @@ const useStyles = makeStyles((theme) => ({
     fontFamily: theme.typography.fontFamily,
     marginBottom: theme.spacing(4),
   },
-  sectionSocial: {
-    flex: 1,
-    display: 'flex',
-    justifyContent: 'center',
-  },
+  sectionSocial: {},
   itemSocial: {
     paddingLeft: 10,
     paddingRight: 10,
@@ -94,6 +50,13 @@ const useStyles = makeStyles((theme) => ({
       height: 30,
     },
   },
+  tooltip: {
+    backgroundColor: 'rgb(0 0 0 / 40%)',
+    color: '#fff',
+    padding: 5,
+    fontSize: '.9em',
+    fontWeight: '600',
+  },
 }));
 const ItemSocial = ({ children }) => {
   const classes = useStyles();
@@ -108,42 +71,77 @@ const BottomFooter = () => {
   const classes = useStyles();
   return (
     <>
-      <Grid className={classes.bottomBar}>
-        <Grid className={classes.section}>
+      <Grid
+        container
+        alignItems="center"
+        wrap="wrap"
+        className={classes.bottomBar}
+      >
+        <Grid
+          container
+          item
+          md={4}
+          justify="center"
+          className={classes.section}
+        >
           <Typography className={classes.bottomText}>
             Copyright 2020 Korova Digital All Rights Reserved.
           </Typography>
         </Grid>
-        <Grid className={classes.sectionSocial}>
+        <Grid container item md={4} justify="center">
           <Typography className={classes.bottomText}>
             Email: info@korovadigital.com
           </Typography>
         </Grid>
-        <Grid className={classes.sectionSocial}>
-          <Link
-            className={classes.link}
-            to="https://www.facebook.com/korovadigital/"
-          >
-            <ItemSocial>
-              <FacebookIcon />
-            </ItemSocial>
-          </Link>
-          <Link
-            className={classes.link}
-            to="https://www.instagram.com/korovadigital/"
-          >
-            <ItemSocial>
-              <InstagramIcon />
-            </ItemSocial>
-          </Link>
-          <Link
-            className={classes.link}
-            to="https://github.com/obsidian-software-service/korova-website"
-          >
-            <ItemSocial>
-              <GitHubIcon />
-            </ItemSocial>
-          </Link>
+        <Grid container item md={4} justify="center">
+          <Tooltip title="Youtube" classes={classes}>
+            <Grid item xs={2}>
+              <Link
+                className={classes.link}
+                to="https://www.facebook.com/korovadigital/"
+              >
+                <ItemSocial>
+                  <YouTubeIcon />
+                </ItemSocial>
+              </Link>
+            </Grid>
+          </Tooltip>
+          <Tooltip title="Facebook" classes={classes}>
+            <Grid item xs={2}>
+              <Link
+                className={classes.link}
+                to="https://www.facebook.com/korovadigital/"
+              >
+                <ItemSocial>
+                  <FacebookIcon />
+                </ItemSocial>
+              </Link>
+            </Grid>
+          </Tooltip>
+          <Tooltip title="Instagram" classes={classes}>
+            <Grid item xs={2}>
+              <Link
+                className={classes.link}
+                to="https://www.instagram.com/korovadigital/"
+              >
+                <ItemSocial>
+                  <InstagramIcon />
+                </ItemSocial>
+              </Link>
+            </Grid>
+          </Tooltip>
+          <Tooltip title="GitHub" classes={classes}>
+            <Grid item xs={2}>
+              <Link
+                className={classes.link}
+                to="https://github.com/obsidian-software-service/korova-website"
+              >
+                <ItemSocial>
+                  <GitHubIcon />
+                </ItemSocial>
+              </Link>
+            </Grid>
+          </Tooltip>
         </Grid>
       </Grid>
     </>
